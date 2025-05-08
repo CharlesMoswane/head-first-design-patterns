0b1c4a1e7699f427d6a1e0142240a02cb2b2ad06
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-05-08 18:31:29 +0200

    feat: Add duck member variables and and getters and setters

--- src/main/java/chapter1/Duck.java
+++ src/main/java/chapter1/Duck.java
@@ -1,4 +1,22 @@
 package chapter1;
 
 public class Duck {
+    private String quack;
+    private String swim;
+
+    public String getQuack() {
+        return quack;
+    }
+
+    public void setQuack(String quack) {
+        this.quack = quack;
+    }
+
+    public String getSwim() {
+        return swim;
+    }
+
+    public void setSwim(String swim) {
+        this.swim = swim;
+    }
 }
