f88f3225d1092793310e1d7fa8b3b06e13b97cd6
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-05-09 18:41:42 +0200

    feat: Call duck in main class

--- src/main/java/chapter1/Main.java
+++ src/main/java/chapter1/Main.java
@@ -3,5 +3,9 @@ package chapter1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Chapter 1");
+        Duck duck = new Duck();
+
+        duck.quack();
+        duck.swim();
     }
 }
