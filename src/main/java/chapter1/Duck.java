5478f75621bbe12dadc39a3593a9a9c3665fde58
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-05-09 18:36:40 +0200

    feat: Remove duck properties and replace them with methods

--- src/main/java/chapter1/Duck.java
+++ src/main/java/chapter1/Duck.java
@@ -1,22 +1,12 @@
 package chapter1;
 
 public class Duck {
-    private String quack;
-    private String swim;
 
-    public String getQuack() {
-        return quack;
+    public void quack() {
+        System.out.println("Quack");
     }
 
-    public void setQuack(String quack) {
-        this.quack = quack;
-    }
-
-    public String getSwim() {
-        return swim;
-    }
-
-    public void setSwim(String swim) {
-        this.swim = swim;
+    public void swim() {
+        System.out.println("Float like a butterfly");
     }
 }
