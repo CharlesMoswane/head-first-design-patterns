3f9c27da4ef1459c802eeb7b17152a1b1bbe04e2
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-05-10 18:16:38 +0200

    feat: Add new Duck type MallardDuck

--- src/main/java/chapter1/Main.java
+++ src/main/java/chapter1/Main.java
@@ -1,9 +1,15 @@
 package chapter1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Chapter 1");
+        List<Duck> ducks = new ArrayList<>();
         Duck duck = new Duck();
+
+
 
         duck.quack();
         duck.swim();
