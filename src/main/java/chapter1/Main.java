bff261995ff5884abb8281c3db2ffeb98333db52
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-05-10 18:20:21 +0200

    feat: Polymorphically set ducks to quack and swim

--- src/main/java/chapter1/Main.java
+++ src/main/java/chapter1/Main.java
@@ -8,10 +8,14 @@ public class Main {
         System.out.println("Hi Chapter 1");
         List<Duck> ducks = new ArrayList<>();
         Duck duck = new Duck();
+        MallardDuck mallardDuck = new MallardDuck();
 
+        ducks.add(duck);
+        ducks.add(mallardDuck);
 
-
-        duck.quack();
-        duck.swim();
+        for (Duck d: ducks) {
+            duck.quack();
+            duck.swim();
+        }
     }
 }
