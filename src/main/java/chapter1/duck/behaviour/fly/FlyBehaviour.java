5dd8450a7ef4666bdc13a7ff372886b4e07e1f5b
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-06-18 18:26:54 +0200

    feat: Add fly abstract method to FlyBehaviour and implementation in FlyNoWay

--- src/main/java/chapter1/duck/behaviour/fly/FlyBehaviour.java
+++ src/main/java/chapter1/duck/behaviour/fly/FlyBehaviour.java
@@ -1,4 +1,5 @@
 package chapter1.duck.behaviour.fly;
 
 public interface FlyBehaviour {
+    public void fly();
 }
--- src/main/java/chapter1/duck/behaviour/fly/FlyNoWay.java
+++ src/main/java/chapter1/duck/behaviour/fly/FlyNoWay.java
@@ -1,4 +1,8 @@
 package chapter1.duck.behaviour.fly;
 
 public class FlyNoWay implements FlyBehaviour {
+    @Override
+    public void fly() {
+        System.out.println("I cannot fly!");
+    }
 }
