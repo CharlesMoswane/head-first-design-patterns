38d01bb90c3121534d646efd8067f9c4444db5e6
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-05 17:23:33 +0200

    feat: Implement MallardDuck behaviour

--- src/main/java/chapter1/duck/model/MallardDuck.java
+++ src/main/java/chapter1/duck/model/MallardDuck.java
@@ -1,4 +1,13 @@
 package chapter1.duck.model;
 
+import chapter1.duck.behaviour.quack.Quack;
+import chapter1.duck.behaviour.quack.QuackBehaviour;
+import chapter1.duck.behaviour.swim.Swim;
+import chapter1.duck.behaviour.swim.SwimBehaviour;
+
 public class MallardDuck extends Duck{
+
+    public MallardDuck() {
+        super(new Quack(), new Swim());
+    }
 }
