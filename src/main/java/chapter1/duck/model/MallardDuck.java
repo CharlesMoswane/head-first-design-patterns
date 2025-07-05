5325682aca1be496c85027d626deda13243504c3
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-05 17:24:06 +0200

    feat: Remove unused imports

--- src/main/java/chapter1/duck/model/MallardDuck.java
+++ src/main/java/chapter1/duck/model/MallardDuck.java
@@ -1,9 +1,7 @@
 package chapter1.duck.model;
 
 import chapter1.duck.behaviour.quack.Quack;
-import chapter1.duck.behaviour.quack.QuackBehaviour;
 import chapter1.duck.behaviour.swim.Swim;
-import chapter1.duck.behaviour.swim.SwimBehaviour;
 
 public class MallardDuck extends Duck{
 
