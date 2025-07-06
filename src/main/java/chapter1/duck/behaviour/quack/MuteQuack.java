93e4ff71b8b12543d6d36e04207b02fd9e2292c1
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-06 18:43:17 +0200

    feat: Renamed QuackNoQuack to MuteQuack"

--- src/main/java/chapter1/duck/behaviour/quack/MuteQuack.java
+++ src/main/java/chapter1/duck/behaviour/quack/MuteQuack.java
@@ -1,6 +1,6 @@
 package chapter1.duck.behaviour.quack;
 
-public class QuackNoQuack implements QuackBehaviour {
+public class MuteQuack implements QuackBehaviour {
     @Override
     public void quack() {
         System.out.println("I do not quack!");
