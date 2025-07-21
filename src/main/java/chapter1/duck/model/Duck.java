ac762a84ca2d4d73d1213afe5f9629f6cd0794cb
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-21 20:45:53 +0200

    feat: Add FlyBehaviour to Duck

--- src/main/java/chapter1/duck/model/Duck.java
+++ src/main/java/chapter1/duck/model/Duck.java
@@ -1,5 +1,6 @@
 package chapter1.duck.model;
 
+import chapter1.duck.behaviour.fly.FlyBehaviour;
 import chapter1.duck.behaviour.quack.Quack;
 import chapter1.duck.behaviour.quack.QuackBehaviour;
 import chapter1.duck.behaviour.swim.Swim;
@@ -9,6 +10,7 @@ public class Duck {
 
     private QuackBehaviour quackBehaviour;
     private SwimBehaviour swimBehaviour;
+    private FlyBehaviour flyBehaviour;
 
     public Duck() {
         this.quackBehaviour = new Quack();
@@ -42,5 +44,13 @@ public class Duck {
 
     public void setSwimBehaviour(SwimBehaviour swimBehaviour) {
         this.swimBehaviour = swimBehaviour;
+    }
+
+    public FlyBehaviour getFlyBehaviour() {
+        return flyBehaviour;
+    }
+
+    public void setFlyBehaviour(FlyBehaviour flyBehaviour) {
+        this.flyBehaviour = flyBehaviour;
     }
 }
