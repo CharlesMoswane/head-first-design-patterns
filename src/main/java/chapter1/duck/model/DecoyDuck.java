248916be0260ad5f1ed886bf55acc2a6116543d7
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-23 17:18:59 +0200

    feat: Add FlyBehaviour to DecoyDuck constructor

--- src/main/java/chapter1/duck/model/DecoyDuck.java
+++ src/main/java/chapter1/duck/model/DecoyDuck.java
@@ -1,10 +1,11 @@
 package chapter1.duck.model;
 
+import chapter1.duck.behaviour.fly.FlyNoWay;
 import chapter1.duck.behaviour.quack.MuteQuack;
 import chapter1.duck.behaviour.swim.Float;
 
 public class DecoyDuck extends Duck{
     public DecoyDuck() {
-        super(new MuteQuack(), new Float());
+        super(new MuteQuack(), new Float(), new FlyNoWay());
     }
 }
