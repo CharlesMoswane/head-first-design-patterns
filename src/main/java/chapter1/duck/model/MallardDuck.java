d6085926f2384f2333bc77261c9e41ddd86b33bf
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-23 17:20:45 +0200

    feat: Add FlyBehaviour to MallardDuck constructor

--- src/main/java/chapter1/duck/model/MallardDuck.java
+++ src/main/java/chapter1/duck/model/MallardDuck.java
@@ -1,11 +1,12 @@
 package chapter1.duck.model;
 
+import chapter1.duck.behaviour.fly.FlyWithWings;
 import chapter1.duck.behaviour.quack.Quack;
 import chapter1.duck.behaviour.swim.Swim;
 
 public class MallardDuck extends Duck{
 
     public MallardDuck() {
-        super(new Quack(), new Swim());
+        super(new Quack(), new Swim(), new FlyWithWings());
     }
 }
