1beb8b45face38940cc9f796cad1667d3127a92e
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-25 17:57:58 +0200

    feat: Add notify WeatherORama and set it to implement Subject

---
+++ src/main/java/chapter2/weatherstation/subject/WeatherORama.java
@@ -0,0 +1,10 @@
+package chapter2.weatherstation.subject;
+
+import chapter2.weatherstation.observer.Observer;
+
+public class WeatherORama implements Subject{
+    @Override
+    public void notify(Observer observer) {
+
+    }
+}
