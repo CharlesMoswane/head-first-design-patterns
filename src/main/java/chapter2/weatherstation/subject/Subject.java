a3ed78ed2c6fc0b8e7957a8001f305d9baa29aaf
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-27 17:25:19 +0200

    feat: Add method addObserver to Subject interface

--- src/main/java/chapter2/weatherstation/subject/Subject.java
+++ src/main/java/chapter2/weatherstation/subject/Subject.java
@@ -3,5 +3,6 @@ package chapter2.weatherstation.subject;
 import chapter2.weatherstation.observer.Observer;
 
 public interface Subject {
-    public void notify(Observer observer);
+    public void addObserver(Observer observer);
+    public void notify();
 }
