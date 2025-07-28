e6c88995bd827bbbebff5a62c12f4fc489a55ada
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-07-28 22:30:00 +0200

    feat: Add observers list to WeatherORama

--- src/main/java/chapter2/weatherstation/subject/WeatherORama.java
+++ src/main/java/chapter2/weatherstation/subject/WeatherORama.java
@@ -2,10 +2,19 @@ package chapter2.weatherstation.subject;
 
 import chapter2.weatherstation.observer.Observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherORama implements Subject{
+
+    List<Observer> observers;
+
+    public WeatherORama() {
+        this.observers = new ArrayList<>();
+    }
     @Override
     public void addObserver(Observer observer) {
-
+        this.observers.add(observer);
     }
 
     @Override
