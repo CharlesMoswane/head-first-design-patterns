fc1cb3f5a372067d92c5fc1897cf3b77efeb6c4d
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-08-02 17:25:15 +0200

    feat: Implement performAction for WeatherAggregate

--- src/main/java/chapter2/weatherstation/observer/WeatherAggregate.java
+++ src/main/java/chapter2/weatherstation/observer/WeatherAggregate.java
@@ -3,6 +3,6 @@ package chapter2.weatherstation.observer;
 public class WeatherAggregate implements Observer{
     @Override
     public void performAction() {
-
+        System.out.println("I am performing an action!");
     }
 }
