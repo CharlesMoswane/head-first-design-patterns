fcbfc5fd7686c6309a61b104ce53d6b743294b60
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-08-21 17:26:51 +0200

    feat: Add WeatherORama to main method

--- src/main/java/chapter2/Main.java
+++ src/main/java/chapter2/Main.java
@@ -1,7 +1,12 @@
 package chapter2;
 
+import chapter2.weatherstation.subject.WeatherORama;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello chapter, how are your publishers and subscribers?");
+
+        WeatherORama weatherORama = new WeatherORama();
+        weatherORama.notifyObservers();
     }
 }
