abb19c15f69288e168ea3ddbf5b7365eed4e1ff4
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-08-21 17:20:43 +0200

    refactor: Rename Main to Duck in duck package

--- src/main/java/chapter1/Duck.java
+++ src/main/java/chapter1/Duck.java
@@ -1,18 +1,17 @@
 package chapter1;
 
 import chapter1.duck.model.DecoyDuck;
-import chapter1.duck.model.Duck;
 import chapter1.duck.model.MallardDuck;
 import chapter1.duck.model.RubberDuck;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Duck {
     public static void main(String[] args) {
         System.out.println("Hi Chapter 1");
-        List<Duck> ducks = new ArrayList<>();
-        Duck duckOg = new Duck();
+        List<chapter1.duck.model.Duck> ducks = new ArrayList<>();
+        chapter1.duck.model.Duck duckOg = new chapter1.duck.model.Duck();
         MallardDuck mallardDuck = new MallardDuck();
         RubberDuck rubberDuck = new RubberDuck();
         DecoyDuck decoyDuck = new DecoyDuck();
@@ -22,7 +21,7 @@ public class Main {
         ducks.add(rubberDuck);
         ducks.add(decoyDuck);
 
-        for (Duck duck: ducks) {
+        for (chapter1.duck.model.Duck duck: ducks) {
             duck.quack();
             duck.swim();
         }
