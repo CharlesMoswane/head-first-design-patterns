1b957c2684660767829ffec2ebd18fb18437576b
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-08-22 17:40:00 +0200

    feat: Add WeatherAggregate observer

--- src/main/java/chapter2/Main.java
+++ src/main/java/chapter2/Main.java
@@ -1,12 +1,17 @@
 package chapter2;
 
+import chapter2.weatherstation.observer.WeatherAggregate;
 import chapter2.weatherstation.subject.WeatherORama;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello chapter, how are your publishers and subscribers?");
 
+        WeatherAggregate weatherAggregate = new WeatherAggregate();
+
         WeatherORama weatherORama = new WeatherORama();
+
+        weatherORama.addObserver(weatherAggregate);
         weatherORama.notifyObservers();
     }
 }
