5436e6f82e13db03d6c6335c0efc2c0b9f76b600
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2025-09-01 17:34:58 +0200

    feat: Add removeObserver to Subject

--- src/main/java/chapter2/weatherstation/subject/Subject.java
+++ src/main/java/chapter2/weatherstation/subject/Subject.java
@@ -4,6 +4,6 @@ import chapter2.weatherstation.observer.Observer;
 
 public interface Subject {
     public void addObserver(Observer observer);
-
+    public void removeObserver(Observer observer);
     public void notifyObservers();
 }
